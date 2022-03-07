58806c937af6c8ea0f83f6ac7ae45f3715ea8bc2
gamejoye <88575063+gamejoye@users.noreply.github.com>
2022-03-07 20:51:52 +0800

    Update so1.java

--- sk的副本/so1.java
+++ sk的副本/so1.java
@@ -17,9 +17,7 @@ public class so1 {
      // 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
      InputStream inputStream = socket.getInputStream();
      byte[] bytes;
-     // 因为可以复用Socket且能判断长度，所以可以一个Socket用到底
      while (true) {
-       // 首先读取两个字节表示的长度
        int first = inputStream.read();
        //如果读取的值为-1 说明到了流的末尾，Socket已经被关闭了，此时将不能再去读取
        if(first==-1){
@@ -27,9 +25,7 @@ public class so1 {
        }
        int second = inputStream.read();
        int length = (first << 8) + second;
-       // 然后构造一个指定长的byte数组
        bytes = new byte[length];
-       // 然后读取指定长度的消息即可
        inputStream.read(bytes);
        System.out.println("get message from client: " + new String(bytes, "UTF-8"));
      }
